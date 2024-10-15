0c080c1e8411701497296d1d8a634cb8fa63a01a
tavneetkhurana-cg <tavneet-singh.khurana@capgemini.com>
2024-10-15 20:11:55 +0530

    Refactor service classes and add repository dependencies
    - Refactor TaskServiceImpl class to include getAllTasks, createTask, and updateTask methods
    - Add TaskRepository dependency to TaskServiceImpl constructor
    - Implement UserService interface in UserServiceImpl class
    - Add UserRepository dependency to UserServiceImpl constructor
    - Implement registerUser and login methods in UserServiceImpl

--- tms/src/main/java/com/capg/service/UserServiceImpl.java
+++ tms/src/main/java/com/capg/service/UserServiceImpl.java
@@ -1,5 +1,31 @@
 package com.capg.service;
 
-public class UserServiceImpl {
+import com.capg.entity.User;
+import com.capg.repository.UserRepository;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User registerUser(User user) {
+        // Save the user to the repository
+        return userRepository.save(user);
+    }
+
+    @Override
+    public String login(User user) {
+        // Validate user credentials
+        User foundUser = userRepository.findByUsername(user.getUsername());
+        if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
+            return "Login successful";
+        } else {
+            return "Invalid username or password";
+        }
+    }
 
 }
